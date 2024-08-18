88ee11d2d2538fb0db5aa958c8be12ba086f4bf8
Omkar Chaudhari <144653123+Omkarc78@users.noreply.github.com>
2024-08-18 19:13:13 +0530

    Add files via upload

---
+++ pattern1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int p=0;
+    for(int i=1;i<=5;i+=2)
+    {
+        for(int k=5;k>=p;k--)
+        {
+            cout<<" ";
+            p++;
+        }
+        for(int j=0;j<i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<"\n";
+    }
+    return 0;
+}
